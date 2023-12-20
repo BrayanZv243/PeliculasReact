4c01f80667bdecf5bc0d054e51d1553901d2a4d4
Brayan Zavala <brayan.zavala228311@potros.itson.edu.mx>
2023-12-20 01:15:56 -0700

    changes

---
+++ peliculas_api/Models/Pelicula.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace peliculas_api.Models
+{
+    public class Pelicula
+    {
+        [Key]
+        public int IdPelicula { get; set; }
+        public string Titulo { get; set; }
+        public int Anio { get; set; }
+        public int Duracion { get; set; }
+        public string Genero { get; set; }
+        public string Director { get; set; }
+        public string Actores { get; set; }
+        public string Sinopsis { get; set; }
+        public string Portada { get; set; }
+        public byte Estrellas { get; set; }
+
+        [Column(TypeName = "decimal(18, 6)")]
+        public decimal Precio { get; set; }
+        public List<Favorito> Favorito { get; set; }
+        public List<Carrito> Carrito { get; set; }
+
+    }
+}
